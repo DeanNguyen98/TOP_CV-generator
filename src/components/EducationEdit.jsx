4a7ac863ee13ac736a31dd357a93089c8d890985
DeanNguyen98 <nctuan.hn@gmail.com>
2024-06-14 23:07:31 +1000

    Fix naming for easy read

--- src/components/EducationEdit.jsx
+++ src/components/EducationEdit.jsx
@@ -10,7 +10,7 @@ export function EducationEditor({onAddItem, EduData, onEditItem}) {
         endDate: '',
         location: '',
     })
-    function handleBasicEduChange (e) {
+    function handleAddEduObject (e) {
         const {name, value} = e.target;
         setEduObject({
           ...EduObject, [name]: value
@@ -38,23 +38,23 @@ export function EducationEditor({onAddItem, EduData, onEditItem}) {
             }}>
                 <label className="form-label" htmlFor="Organisation">
                     <span>Organisation</span>
-                    <input type="text" value={EduObject.Organisation} onChange={handleBasicEduChange} id="Organisation" name="Organisation"></input>
+                    <input type="text" value={EduObject.Organisation} onChange={handleAddEduObject} id="Organisation" name="Organisation"></input>
                 </label>
                 <label className="form-label" htmlFor="degree">
                     <span>Degree</span>
-                    <input type="text" id="degree" value={EduObject.degree} onChange={handleBasicEduChange} name="degree"></input>
+                    <input type="text" id="degree" value={EduObject.degree} onChange={handleAddEduObject} name="degree"></input>
                 </label>
                 <label className="form-label" htmlFor="startDate">
                     <span>Start Date</span>
-                    <input type="month" id="startDate" value={EduObject.startDate} onChange={handleBasicEduChange}  name="startDate"></input>
+                    <input type="month" id="startDate" value={EduObject.startDate} onChange={handleAddEduObject}  name="startDate"></input>
                 </label>
                 <label className="form-label" htmlFor="endDate">
                     <span>End Date</span>
-                    <input type="month" id="endDate" value={EduObject.endDate} onChange={handleBasicEduChange} name="endDate"></input>
+                    <input type="month" id="endDate" value={EduObject.endDate} onChange={handleAddEduObject} name="endDate"></input>
                 </label>
                 <label className="form-label" htmlFor="location">
                     <span>Location</span>
-                    <input type="text" id="location" value={EduObject.location} onChange={handleBasicEduChange} name="location"></input>
+                    <input type="text" id="location" value={EduObject.location} onChange={handleAddEduObject} name="location"></input>
                 </label>
                 <button type="submit">Submit</button>
             </form>
