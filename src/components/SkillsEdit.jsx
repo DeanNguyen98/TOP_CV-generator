69ca71f8f47857c897da0e06cd3b04e07a46ba66
DeanNguyen98 <nctuan.hn@gmail.com>
2024-06-15 22:26:43 +1000

    Apply the same function as ExperienceEdit and EducationEdit. create a new SkillObject for each skill, and when the object is added to the array, an id is generated using uuidv4

--- src/components/SkillsEdit.jsx
+++ src/components/SkillsEdit.jsx
@@ -1,10 +1,39 @@
-export function SkillsEditor () {
+import {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import ItemForm from './ItemForm';
+export function SkillsEditor ({SkillData, onEditItem, onAddItem}) {
+    const [skillObject, setSkillObject] = useState({
+        skill:''
+    });
+    function handleSkillEdit (e) {
+        const {name, value} = e.target
+        setSkillObject({
+            ...skillObject, [name]: value
+        })
+    }
     return (
-        <form className="form-container">
-            <label className="form-label" htmlFor="skills">
+        <>
+        {SkillData.length > 0 && SkillData.map(data => (
+            <ItemForm 
+                key={data}
+                item={data}
+                onSave={onEditItem}
+            />
+        ))}
+
+        <form className="form-container" onSubmit={(e) => {
+            e.preventDefault();
+            onAddItem({...skillObject, id: uuidv4()});
+            setSkillObject({
+                skill: ''
+            })
+        }}>
+            <label className="form-label" htmlFor="skill">
                 <span>Skills</span>
-                <input type="text" id="skills" name="skills"></input>
+                <input type="text" id="skill" value={skillObject.skill} name="skill" onChange={handleSkillEdit}></input>
             </label>
+            <button type="submit">Add skill</button>
         </form>
+        </>
     )
 }
