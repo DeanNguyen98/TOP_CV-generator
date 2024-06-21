d21a9b20f31b94c402d0be6a5a02feafc01ff5c5
DeanNguyen98 <nctuan.hn@gmail.com>
2024-06-21 20:24:38 +1000

    Download PDF file button

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,6 +3,7 @@ import { EducationEditor } from "./EducationEdit";
 import { ExperienceEditor } from "./ExperienceEdit";
 import { SkillsEditor } from "./SkillsEdit";
 import '../styles/editor.scss'
+import { downloadCV } from "./CVpreview/CvPDF";
 
 export default function MainEditor(props) {
     const {
@@ -26,6 +27,7 @@ export default function MainEditor(props) {
     return (
         <div className="Editor-side">
             <h1>CV Generator</h1>
+            <button onClick={downloadCV}><i className="fa-solid fa-file-pdf"></i>&nbsp;Download PDF</button>
             <BasicInfoEdit data={BasicData} handleChange={handleInfoChange} />
             <EducationEditor EduData={EduData} onEditItem={handleEduEdit} onAddItem={handleEduChange} onRemoveItem={handleEduRemove}/>
             <ExperienceEditor ExpData = {ExpData} onAddItem={handleExpAdd} onEditItem={handleExpEdit} onRemoveItem={handleExpRemove} />
